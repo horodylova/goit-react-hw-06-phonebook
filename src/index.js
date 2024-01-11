4d2943bc4b998f35cf302958192bb7f6fe572c05
svitlana-horodylova <horodylova.sv@gmail.com>
2024-01-11 12:34:06 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import theme from './styles/theme';
@@ -13,11 +12,9 @@ createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-06-phonebook">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
-        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
