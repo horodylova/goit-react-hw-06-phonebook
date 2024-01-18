9d3df0ebd81234bb747551e8c4f33f3be3d15ce3
svitlana-horodylova <horodylova.sv@gmail.com>
2024-01-18 16:43:08 +0000

    Moved states from App to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,19 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { AppContainer, Heading1, Heading2 } from './App.styled';
 
 export const App = () => {
-  const contacts = useSelector((state) => state.contacts.contacts);
-  const filter = useSelector((state) => state.contacts.filter);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-  }, [dispatch]);
+  
 
   return (
     <AppContainer>
     <Heading1>Phonebook</Heading1>
     <ContactForm />
     <Heading2>Contacts</Heading2>
-    <Filter value={filter} />
-    <ContactList contacts={contacts} />
+    <Filter />
+    <ContactList/>
   </AppContainer>
 
   );
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { ContactItem } from '../ContactItem/ContactItem';
+import { useSelector } from 'react-redux';
 
-export const ContactList = ({ contacts, onDelete }) => (
-    <ul>
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} onDelete={onDelete} />
-      ))}
-    </ul>
-  );
+export const ContactList = () => {
+  const contacts = useSelector((state) => state.contacts.contacts);
+  const filter = useSelector((state) => state.contacts.filter);
+
+  const filteredContacts = contacts.filter((contact) =>
+  contact.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const renderContacts = () => {
+    if (filteredContacts.length === 0) {
+      return <p>No contacts found</p>;
+    }
+
+    return (
+      <ul>
+        {filteredContacts.map((contact) => (
+          <ContactItem key={contact.id} contact={contact} />
+        ))}
+      </ul>
+    );
+  };
+
+  return <div>{renderContacts()}</div>;
+};
+
+    
+ 
   
 
   
